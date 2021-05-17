5007d1ca8c56c27140a9e88752c00853052314f4
alextran0899 <alextran@MacBook-Air.local>
2021-05-17 18:03:01 -0500

    cleaned 1 6 pm

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,8 +6,8 @@ const messageSchema = yup.object({
   password: yup.string().required("/ must be longer than 3/i").min(3, "/ must be longer than 3/i")
 })
 
-function restricted(req, res,next) {
-  if(req.session.user) {
+function restricted(req, res, next) {
+  if (req.session.user) {
     next()
   } else {
     next({
@@ -16,52 +16,34 @@ function restricted(req, res,next) {
   }
 }
 
-function checkUsernameFree(req, res,next){
-  const {username} = req.body
-  User.findBy({username})
-  .then(user => {
-    if(!user) {
-      next()
-    } else {
-      next({status: 422, message: "username taken"})
-    }
-  })
-  .catch(err => {
-    next(err)
-  })
+function checkUsernameFree(req, res, next) {
+  const { username } = req.body
+  User.findBy({ username })
+    .then(user => {
+      if (!user) {
+        next()
+      } else {
+        next({ status: 422, message: "username taken" })
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 function checkUsernameExists() {
 
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-async function checkPasswordLength(req, res,next) {
+async function checkPasswordLength(req, res, next) {
   try {
     const validate = await messageSchema.validate(req.body, { stripUnknown: true })
     req.body = validate
     next()
-} catch (err) {
+  } catch (err) {
     next({ status: 422, message: err.message })
+  }
 }
-}
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
 
 module.exports = {
   restricted,
--- api/server.js
+++ api/server.js
@@ -13,18 +13,18 @@ const sessionConfig = {
   secret: 'keep it secret, keep it safe!',
   cookie: {
     maxAge: 1000 * 60 * 60,
-    secure: false, // if true the cookie is not set unless it's an https connection
-    httpOnly: false, // if true the cookie is not accessible through document.cookie
+    secure: false, 
+    httpOnly: false, 
   },
   rolling: true,
-  resave: false, // some data stores need this set to true
-  saveUninitialized: false, // privacy implications, if false no cookie is set on client unless the req.session is changed
+  resave: false, 
+  saveUninitialized: false,
   store: new KnexSessionStore({
-    knex: require('../data/db-config'), // configured instance of knex
-    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
-    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
-    createtable: true, // if the table does not exist, it will create it automatically
-    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+    knex: require('../data/db-config'), 
+    tablename: 'sessions', 
+    sidfieldname: 'sid',
+    createtable: true, 
+    clearInterval: 1000 * 60 * 60,
   }),
 };
 
@@ -36,7 +36,7 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
